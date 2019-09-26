34f71164f6d51f83267b7dc4af416a9864657102
Zainul Franciscus <Zainul.Franciscus@myob.com>
2019-09-26 20:55:25 +1000

    copy convent of vec inside fill_vec

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,9 +2,8 @@
 // Make me compile without changing line 10! Scroll down for hints :)
 
 fn main() {
-    let vec0 = Vec::new();
-    let copyOfVec0 = vec0.to_vec();
-    let mut vec1 = fill_vec( copyOfVec0);
+    let mut vec0 = Vec::new();
+    let mut vec1 = fill_vec( &vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -14,8 +13,8 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec:  Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec:  &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.to_vec();
 
     vec.push(22);
     vec.push(44);
