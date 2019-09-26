e81950793b21f2ca32ded68b20f1afd58c1a0ad3
Zainul Franciscus <Zainul.Franciscus@myob.com>
2019-09-26 20:52:10 +1000

    copy vec0 and pass it to this function

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,9 +2,9 @@
 // Make me compile without changing line 10! Scroll down for hints :)
 
 fn main() {
-    let mut vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(&mut vec0);
+    let vec0 = Vec::new();
+    let copyOfVec0 = vec0.to_vec();
+    let mut vec1 = fill_vec( copyOfVec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -14,14 +14,14 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+fn fill_vec(vec:  Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
 
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec.to_vec()
+    vec
 }
 
 
